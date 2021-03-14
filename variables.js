1b9c6806a233a65fd36a17f21ebdef9b2ffe7932
ErikMarkosyan <er001.em@gmail.com>
2021-03-14 19:48:42 +0400

    Here I added some info related to wariables and show my own examples

---
+++ variables.js
@@ -0,0 +1,36 @@
+// we can declare variables only with`
+// let myName
+// let car123
+// let $somelet
+// let _underline
+
+var myName1 = 'somename'; //old style of declaration
+let myName2 = 'somename'; //modern style of declaration
+
+// when we change the value of previous variable by declaratin another one 
+
+let myName = 'unknown'; // this will change the old value
+
+const birthDay = '06.04.1997' // the value of const cannot be changed, if we change it, we will get an error
+
+const name = 'Erik', lastName = 'Markosyan'
+
+// We can also declare two variables and copy data from one into the other.
+
+let device1 = 'Apple'
+let device2 = 'PlayStation'
+
+device1 = device2;
+
+console.log(device1); //this will return 'PlayStation', we changed value of device1 from 'Apple' to 'PlayStation'
+
+// Summary
+// We can declare variables to store data by using the var, let, or const keywords.
+
+// var – is an old-school variable declaration.
+// let – is a modern variable declaration.
+// const – is like let, but the value of the variable can’t be changed.
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
