1329cfc6d17fcaeb09a5b67da1d7d2fcad14d3d0
ErikMarkosyan <er001.em@gmail.com>
2021-03-15 11:00:30 +0400

    Here was added boolean conversion

--- type_conversions.js
+++ type_conversions.js
@@ -34,3 +34,40 @@ console.log(num, typeof num); // 123 "number"
 let age = Number("an arbitrary string instead of a number");
 
 console.log(age); // NaN, conversion failed
+
+// Numeric conversion rules:
+// undefined becomes NaN
+// null becomes 0
+// true and false becomes 1 and 0
+// string - Whitespaces from the start and end are removed. 
+// If the remaining string is empty, the result is 0. 
+// Otherwise, the number is “read” from the string. An error gives NaN.
+
+console.log( Number("   123   ") ); // 123
+console.log( Number("123z") );      // NaN (error reading a number at "z")
+console.log( Number(true) );        // 1
+console.log( Number(false) );       // 0
+
+// Please note that null and undefined behave differently here: null becomes zero while undefined becomes NaN.
+
+// Boolean Conversion
+// Boolean conversion is the simplest one.
+
+// It happens in logical operations (later we’ll meet condition tests and other similar things) but can also be performed explicitly with a call to Boolean(value).
+
+// The conversion rule:
+
+// Values that are intuitively “empty”, like 0, an empty string, null, undefined, and NaN, become false.
+// Other values become true.
+
+console.log( Boolean(1) ); // true
+console.log( Boolean(0) ); // false
+console.log( Boolean("hello") ); // true
+console.log( Boolean("") ); // false
+console.log( Boolean(null) ); // false
+console.log( Boolean(undefined) ); // false
+
+// the string with zero "0" is true
+
+console.log( Boolean("0") ); // true
+console.log( Boolean(" ") ); // spaces, also true (any non-empty string is true)
