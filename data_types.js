4889c61edf852ea7c3955a70ac86cd7deb44a514
ErikMarkosyan <er001.em@gmail.com>
2021-03-15 10:29:22 +0400

    type_conversions.js file was added

--- data_types.js
+++ data_types.js
@@ -69,3 +69,86 @@ let ageFieldChecked = false; // no, age field is not checked
 // Boolean values also come as a result of comparisons:
 
 let isGreater = 4 > 1; // true
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Null պրծած զուքարանի թուղթ
+// The special null value does not belong to any of the types described above.
+
+// It forms a separate type of its own which contains only the null value:
+
+let age = null;
+// It’s just a special value which represents "nothing", "empty" or "value unknown".
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Undefined որ զուքարանի թողթ չկա 
+
+// The special value undefined also stands apart. It makes a type of its own, just like null.
+
+// The meaning of undefined is "value is not assigned".
+
+// If a variable is declared, but not assigned, then its value is undefined:
+
+let x;
+console.log(x); // undefined
+// Technically, it is possible to assign undefined to any variable:
+
+let x = undefined;
+// …But we don’t recommend doing that. Normally, we use null to assign an "empty" or "unknown" value to a variable,
+//  and we use undefined for checks like seeing if a variable has been assigned.
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Object
+
+// Symbol
+// The symbol type is used to create unique identifiers for objects. 
+// We mention it here for completeness, but we’ll study it after objects.
+
+// The typeof operator
+// The typeof operator returns the type of the argument. 
+// It’s useful when we want to process values of different types differently or just want to do a quick check.
+
+// It supports two forms of syntax:
+
+// As an operator: typeof x.
+// As a function: typeof(x).
+// The call to typeof x returns a string with the type name:
+
+typeof undefined // "undefined"
+
+typeof 0 // "number"
+
+typeof 10n // "bigint"
+
+typeof true // "boolean"
+
+typeof "foo" // "string"
+
+typeof Symbol("id") // "symbol"
+
+typeof null // "object"
+
+typeof function() {} // "function"
+
+// The result of typeof null is "object". That’s wrong. It is an officially recognized error in typeof, 
+// kept for compatibility. Of course, null is not an object. 
+// It is a special value with a separate type of its own. So, again, this is an error in the language.
+
+// Summary
+// There are 8 basic data types in JavaScript.
+
+// number for numbers of any kind: integer or floating-point, integers are limited by ±253.
+// bigint is for integer numbers of arbitrary length.
+// string for strings. A string may have one or more characters, there’s no separate single-character type.
+// boolean for true/false.
+// null for unknown values – a standalone type that has a single value null.
+// undefined for unassigned values – a standalone type that has a single value undefined.
+// object for more complex data structures.
+// symbol for unique identifiers.
+// The typeof operator allows us to see which type is stored in a variable.
+
+// Two forms: typeof x or typeof(x).
+// Returns a string with the name of the type, like "string" or "number".
+// For null returns "object" – this is an error in the language, it’s not actually an object.
